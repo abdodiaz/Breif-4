f11939c060a1c39e9bc46f52588c23c7c08f5518
dazia <abdelbarredazia@gmail.com>
2021-03-16 14:26:24 +0100

    back

--- PHP/insertBooks.php
+++ PHP/insertBooks.php
@@ -1,27 +1,26 @@
 <?php
-$user='root';
-$pass='';
-try {
-$db =new PDO('mysql:host=localhost;dbname=livre',$user,$pass);
-}
-catch(PDOException $e)
-{
-    print "Erreur :" .$e->getMessage() . "<br>";
-    die;
-}
-//get element
-try {
-$name = $_GET['name'];
-$price = $_GET['prix'];
-$date =$_GET['date'];
-$img = $_GET['img'];
-
-$insert ="INSERT INTO `livres`( `Name`, `prix`, `Ddate`, `Img`) VALUES ('$name',$price,'$date','$img')";
-$db->query($insert);
-echo "rak ghaya";
-}
-catch(PDOException $e)
-{
-    print "Erreur :" .$e->getMessage() . "<br>";
-    die;
+include('config.php');
+if (isset($_POST['insert'])) {
+    try {
+        $name = $_POST['name'];
+        $price = $_POST['prix'];
+        $date = $_POST['date'];
+        $img =$_FILES['img']['name'];
+        $upp="../books/".$img;        
+         move_uploaded_file($_FILES['img']['tmp_name'],$upp);
+        $insert = "INSERT INTO `livres`( `Name`, `prix`, `Ddate`, `Img`) VALUES ('$name',$price,'$date','$img')";
+        $db->query($insert);
+        $SelectMaxid="SELECT max(Id) as Id FROM `livres`";
+        $x=$db->query($SelectMaxid);
+        while ($row =$x->fetch()) 
+        {
+          $r= htmlspecialchars($row['Id'])  ;
+        }
+        $selectautId=$_POST['selbooks'];
+        $db->query("INSERT INTO `livaut`(`Idliv`, `Idaut`) VALUES ($r,$selectautId)");
+        header("location: books.php");
+    } catch (PDOException $e) {
+        print "Erreur :" . $e->getMessage() . "<br>";
+        die;
+    }
 }
